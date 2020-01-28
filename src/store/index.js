b77205266361d4c782a398190000338f985f4c9a
quentinaime <quentin.aime.dev@gmail.com>
2020-01-28 13:59:27 +0100

    full setup

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,17 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import actions from "./actions.js";
+import state from "./state.js";
+import mutations from "./mutations.js";
+import getters from "./getters.js";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: state,
+  mutations: mutations,
+  actions: actions,
+  getters: getters,
+  // modules: {}
 });
