1eeb1498cc1c45e041f4c429ce236ff687507996
quentinaime <quentin.aime.dev@gmail.com>
2020-01-28 14:10:05 +0100

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import PointsOfInterest from "../views/PointsOfInterest.vue";
+import Poi from "../views/PointOfInterest.vue";
+import Companies from "../views/Companies.vue";
+import Company from "../views/Company.vue";
+// import Login from "../views/Login.vue";
 
 Vue.use(VueRouter);
 
@@ -10,14 +15,30 @@ const routes = [
     name: "home",
     component: Home
   },
+  // {
+  //   path: "/login",
+  //   name: "login",
+  //   component: Login
+  // },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/poi-list",
+    name: "poi-list",
+    component: PointsOfInterest
+  },
+  {
+    path: "/poi/:uid",
+    name: "poi",
+    component: Poi
+  },
+  {
+    path: "/company-list",
+    name: "company-list",
+    component: Companies
+  },
+  {
+    path: "/company/:uid",
+    name: "company",
+    component: Company
   }
 ];
 
